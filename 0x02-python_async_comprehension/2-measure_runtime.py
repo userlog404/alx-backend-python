dce49ae89dcfeb3550551a414477731b2a595f21
userlog404 <101346110+userlog404@users.noreply.github.com>
2023-02-07 10:58:44 +0300

    t

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+
 """ Measure time"""
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
-
 
 async def measure_runtime() -> float:
     """ measure execution time"""
