02a2df1101ebf4d2c5ea21a35f11404a181e0062
userlog404 <101346110+userlog404@users.noreply.github.com>
2023-02-07 10:58:25 +0300

    t

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+
 """generate a list from a async comprehension"""
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
-
 
 async def async_comprehension() -> List[float]:
     """collect async generated list and return it"""
